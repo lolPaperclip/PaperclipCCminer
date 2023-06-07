6e374fc15af870791717109f2246fbff0b48df9a
lolPaperclip <134574769+lolPaperclip@users.noreply.github.com>
2023-06-06 19:44:20 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,7 @@ EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
-wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-2/ccminer
-wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-2/ccminer-v3.8.3-oink_Ubuntu_18.04
+wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-2/ccminer-v3.8.3-Pangz_ARM
 wget https://raw.githubusercontent.com/lolPaperclip/PaperclipCCminer/main/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
